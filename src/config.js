6ddcc8604d037c7631d0629da6e01e5c04b8fde2
Muskan Afzal <148281538+MuskanAfzal@users.noreply.github.com>
2025-06-22 00:01:13 +0500

    edited config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 // src/config.js
 const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://notes-backend-production-f8ab.up.railway.app' // Replace this when deploying
-    : 'http://localhost:8080';
+  process.env.REACT_APP_BASE_URL ||
+  (process.env.NODE_ENV === 'production'
+    ? 'https://notes-backend-production-f8ab.up.railway.app'
+    : 'http://localhost:8080');
 
 export default BASE_URL;
