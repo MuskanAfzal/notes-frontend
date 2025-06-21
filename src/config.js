c8be6bde0a5216992aadc31263cea1d835d639e8
Muskan Afzal <148281538+MuskanAfzal@users.noreply.github.com>
2025-06-21 23:54:22 +0500

    added bachend base link

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://your-production-api-url.com' // Replace this when deploying
+    ? 'https://notes-backend-production-f8ab.up.railway.app' // Replace this when deploying
     : 'http://localhost:8080';
 
 export default BASE_URL;
